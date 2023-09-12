8b34a1a61fb162a0761b9c0817b2c90fe33cfe2a
s-atyam <satyambharti080@gmail.com>
2023-09-12 16:01:40 +0530

    modules

---
+++ operations.js
@@ -0,0 +1,23 @@
+// This is operations module (file)
+
+const add = (a,b) => {
+    return a+b;
+}
+
+const sub = (a,b) => {
+    return a-b;
+}
+
+const mul = (a,b) => {
+    return a*b;
+}
+
+const div = (a,b) => {
+    return a/b;
+}
+
+const power = (a,b) => {
+    return a**b;
+}
+
+module.exports = {add,sub,mul,div,power};
